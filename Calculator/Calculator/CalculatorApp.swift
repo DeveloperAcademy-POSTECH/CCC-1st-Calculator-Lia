e92d7b6f8df6c14ddf0bf14241bc613f6266528f
Lia316 <kjk0825@postech.ac.kr>
2022-05-28 15:35:56 +0900

    initial commit

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 리아 on 2022/05/28.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
